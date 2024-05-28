de01159b341a5733813a6dddaa0a2c1fd9a25190
Lumiert <bernardogallio@gmail.com>
2024-05-28 17:49:15 -0300

    AAAA

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('node:path');
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true, // Permitir a integração com Node.js
+      contextIsolation: false, // Desativar o isolamento de contexto para acessar arquivos locais
+    },
+  });
+
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
